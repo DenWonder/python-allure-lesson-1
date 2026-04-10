978beb0407ad45fd0c97e1360e349392691f3396
Denis Chudnov PC <denis.chudnov.97@gmail.com>
2026-04-10 17:46:57 +0200

    upd conftest

--- conftest.py
+++ conftest.py
@@ -1,34 +1,33 @@
-import pytest
 import os
-
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-
 from utils import attach
 
+
 @pytest.fixture(scope='function')
-def setup_browser(request):
+def setup_browser():
     options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    if os.getenv("CI"):  # GitHub Actions автоматически выставляет CI=true
+    selenoid_url = os.getenv("SELENOID_URL")
+
+    if os.getenv("CI"):
         options.add_argument("--headless")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
-    # driver = webdriver.Remote(
-    #     # command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
-    #     options=options
-    # )
-    driver = webdriver.Chrome(options=options)
+    elif selenoid_url:
+        options.capabilities.update({
+            "browserName": "chrome",
+            "browserVersion": "100.0",
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
+        })
+
+    driver = webdriver.Remote(command_executor=selenoid_url, options=options) \
+        if selenoid_url else webdriver.Chrome(options=options)
 
     browser = Browser(Config(driver))
     yield browser
@@ -36,6 +35,4 @@ def setup_browser(request):
     attach.add_screenshot(browser)
     attach.add_logs(browser)
     attach.add_html(browser)
-    # attach.add_video(browser)
-
     browser.quit()
