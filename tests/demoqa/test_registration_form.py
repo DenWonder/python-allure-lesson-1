ca181ec7ee4bfd47100653fda722d1c22a7ed239
Denis Chudnov PC <denis.chudnov.97@gmail.com>
2026-04-10 13:55:40 +0200

    Add a couple of tests just to learn how to work with allure reports. Write yaml file to run allure reports by github action

---
+++ tests/demoqa/test_registration_form.py
@@ -0,0 +1,165 @@
+from time import sleep
+
+import allure
+from selene import have, command
+from selene.support.shared import browser
+from selene.support import by
+
+@allure.feature("Форма регистрации")
+@allure.story("Успешное заполнение всех полей")
+@allure.label("owner", "D Chudnov")
+@allure.link("https://demoqa.com", name="Testing")
+def test_form_allure_dynamic_steps():
+    with allure.step("Open registrations form"):
+        browser.open("https://demoqa.com/automation-practice-form")
+        browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
+
+    with allure.step("Fill registration form data"):
+        browser.element("#firstName").type("Frodo")
+        browser.element("#lastName").type("Baggins")
+        browser.element("#userEmail").type("bagginsFr@shire.com")
+        browser.element("#gender-radio-1").click()
+        browser.element("#userNumber").type("1231231231")
+
+        browser.element("#dateOfBirthInput").click()
+        browser.element('.react-datepicker__month-select').all('option') \
+            .element_by(have.exact_text("July")).click()
+        browser.element('.react-datepicker__year-select').all('option') \
+            .element_by(have.exact_text(str(2000))).click()
+        browser.element(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click()
+
+        browser.element("#subjectsInput").send_keys("Maths")
+        browser.element("#subjectsInput").press_enter()
+        browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
+
+        browser.element("#currentAddress").set_value("Some street 1")
+        browser.element("#state").click()
+        browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+        browser.element("#city").click()
+        browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+        browser.element('#submit').perform(command.js.scroll_into_view)
+        browser.element('#submit').perform(command.js.click)
+
+
+    with allure.step("Check form results"):
+        browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
+        browser.element('.table').all('td').even.should(have.exact_texts(
+            'Frodo Baggins',
+            'bagginsFr@shire.com',
+            'Male',
+            '1231231231',
+            '30 July,2000',
+            'Maths',
+            'Sports',
+            '',
+            'Some street 1',
+            'NCR Delhi'
+        ))
+
+@allure.feature("Форма регистрации")
+@allure.story("Динамические шаги")
+@allure.label("owner", "D Chudnov")
+@allure.link("https://demoqa.com", name="Testing")
+def test_form_allure_dynamic_steps_failed():
+    with allure.step("Open registrations form"):
+        browser.open("https://demoqa.com/automation-practice-form")
+        browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
+
+    with allure.step("Fill registration form data"):
+        browser.element("#firstName").type("Frodo")
+        browser.element("#lastName").type("Baggins")
+        browser.element("#userEmail").type("bagginsFr@shire.com")
+        browser.element("#gender-radio-1").click()
+        browser.element("#userNumber").type("1231231231")
+
+        browser.element("#dateOfBirthInput").click()
+        browser.element('.react-datepicker__month-select').all('option') \
+            .element_by(have.exact_text("July")).click()
+        browser.element('.react-datepicker__year-select').all('option') \
+            .element_by(have.exact_text(str(2000))).click()
+        browser.element(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click()
+
+        browser.element("#subjectsInput").send_keys("Maths")
+        browser.element("#subjectsInput").press_enter()
+        browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
+
+        browser.element("#currentAddress").set_value("Some street 1")
+        browser.element("#state").click()
+        browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+        browser.element("#city").click()
+        browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+        browser.element('#submit').perform(command.js.scroll_into_view)
+        browser.element('#submit').perform(command.js.click)
+
+
+    with allure.step("Check form results"):
+        browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
+        browser.element('.table').all('td').even.should(have.exact_texts(
+            'Bilbo Baggins',
+            'bagginsBb@shire.com',
+            'Male',
+            '321321321321',
+            '12 June,1950',
+            'Maths',
+            'Sports',
+            '',
+            'Some street 4',
+            'NCR Delhi'
+        ))
+
+@allure.link("https://demoqa.com", name="Testing")
+@allure.feature("Форма регистрации")
+@allure.story("Разные пользователи в ассерте и тесте")
+@allure.label("owner", "Чуднов")
+def test_form_allure_decorator_steps():
+    open_registrations_form()
+    fill_registration_form_data()
+    check_results()
+
+@allure.step("Open registrations form")
+def open_registrations_form():
+    browser.open("https://demoqa.com/automation-practice-form")
+    browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
+
+@allure.step("Fill registration form data")
+def fill_registration_form_data():
+    browser.element("#firstName").type("Frodo")
+    browser.element("#lastName").type("Baggins")
+    browser.element("#userEmail").type("bagginsFr@shire.com")
+    browser.element("#gender-radio-1").click()
+    browser.element("#userNumber").type("1231231231")
+
+    browser.element("#dateOfBirthInput").click()
+    browser.element('.react-datepicker__month-select').all('option') \
+            .element_by(have.exact_text("July")).click()
+    browser.element('.react-datepicker__year-select').all('option') \
+            .element_by(have.exact_text(str(2000))).click()
+    browser.element(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click()
+
+    browser.element("#subjectsInput").send_keys("Maths")
+    browser.element("#subjectsInput").press_enter()
+    browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
+
+    browser.element("#currentAddress").set_value("Some street 1")
+    browser.element("#state").click()
+    browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+    browser.element("#city").click()
+    browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+    browser.element('#submit').perform(command.js.scroll_into_view)
+    browser.element('#submit').perform(command.js.click)
+
+@allure.step("Check results")
+def check_results():
+    browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
+    browser.element('.table').all('td').even.should(have.exact_texts(
+            'Frodo Baggins',
+            'bagginsFr@shire.com',
+            'Male',
+            '1231231231',
+            '30 July,2000',
+            'Maths',
+            'Sports',
+            '',
+            'Some street 1',
+            'NCR Delhi'
+    ))
--- tests/simple/test_fail.py
+++ tests/simple/test_fail.py
@@ -1,20 +1,28 @@
+import allure
+from allure_commons.types import Severity
+
 def test_fail1():
     assert False
 
 def test_fail2():
     assert False
 
+@allure.severity(Severity.BLOCKER)
 def test_fail3():
     assert False
 
+@allure.severity(Severity.CRITICAL)
 def test_fail4():
     assert False
 
+@allure.severity(Severity.NORMAL)
 def test_fail5():
     assert False
 
+@allure.severity(Severity.MINOR)
 def test_fail6():
     assert False
 
+@allure.severity(Severity.TRIVIAL)
 def test_fail7():
     assert False
