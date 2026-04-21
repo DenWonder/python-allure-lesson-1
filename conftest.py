1ac872611582e279ad86be66f2a4049c8a60ce43
Denis Chudnov PC <denis.chudnov.97@gmail.com>
2026-04-21 15:43:35 +0200

    Update conftest for attachment video

--- conftest.py
+++ conftest.py
@@ -9,29 +9,29 @@ from utils import attach
 
 @pytest.fixture(scope='function')
 def setup_browser():
-    options = Options()
     selenoid_url = os.getenv("SELENOID_URL")
-
-    if os.getenv("CI"):
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--window-size=1920,1080")
-    elif selenoid_url:
-        options.set_capability("selenoid:options", {
-            "enableVNC": True,
-            "enableVideo": True,
-            "videoName": f"{browser.driver.session_id}.mp4" if hasattr(browser, 'driver') else "video.mp4"
-        })
 
     if selenoid_url:
         driver = webdriver.Remote(
             command_executor=selenoid_url,
-            options=options
+            desired_capabilities={
+                "browserName": "chrome",
+                "browserVersion": "128.0",
+                "selenoid:options": {
+                    "enableVNC": True,
+                    "enableVideo": True
+                }
+            }
         )
-        browser.config.driver = driver
+        browser = Browser(Config(driver=driver))
     else:
-        browser.config.driver_options = options
+        options = Options()
+        if os.getenv("CI"):
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--window-size=1920,1080")
+        browser = Browser(Config(driver_options=options))
 
     yield browser
 
@@ -39,7 +39,5 @@ def setup_browser():
     attach.add_logs(browser)
     attach.add_html(browser)
     if selenoid_url:
-        attach.add_video(browser)
-    if os.getenv("SELENOID_URL"):
         attach.add_video(browser)
     browser.quit()
